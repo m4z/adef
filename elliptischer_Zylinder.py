fd9e057df0172d0d93217e7b5ee49c9b1ba15213
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-04-01 01:06:37 +0200

    elliptischer_Zylinder.py aufgeräumt.

--- elliptischer_Zylinder.py
+++ elliptischer_Zylinder.py
@@ -4,28 +4,15 @@ from numpy import sin, cos, pi, mgrid, zeros_like, ones_like
 class elliptischer_Zylinder(myobject):
 	def __init__(self,rx,ry,rz):
 		myobject.__init__(self)
-                #############################################################################
+                ################################################################
                 #parameter fuer die flaeche
 		self.u_f, self.v_f = mgrid[0:2*pi:180j,-1:1:180j]
                 #alle funktionen fuer die flaeche
 		self.x = lambda u,v: rx*cos(u)
 		self.y = lambda u,v: ry*sin(u)
 		self.z = lambda u,v: rz*v
-## 		self.x_f_werte = x_f(myobj.u_f,myobj.v_f)
-## 		self.y_f_werte = y_f(myobj.u_f,myobj.v_f)
-## 		self.z_f_werte = z_f(self.u_f,myobj.v_f)
-		#############################################################################
-##                 #alle funktionen fuer die kurve und die dazugehoerigen funktionen
-##                 self.u = lambda t,tckp : splev(t,tckp,0)[0]
-##                 self.v = lambda t,tckp : splev(t,tckp,0)[1]
-##                 #du,dv sind die ersten Ableitungen der Flaechenparameter.
-##                 self.du = lambda t,tckp : splev(t,tckp,1)[0]
-## 		self.dv = lambda t,tckp : splev(t,tckp,1)[1]
+		################################################################
                 #x,y,z sind die Koordinaten im dreidimensionalem euklidischen Raum
-                #TODO: ueberlegung, ob ich die brauche oder x_f,y_f,z_f reichen
-##                 self.x = lambda u,v: rx*cos(u)*sin(v)
-##                 self.y = lambda u,v: ry*sin(u)*sin(v)
-##                 self.z = lambda u,v: rz*cos(v)
                 #ones_like = einfach die arraywerte in 1 umschreiben.
                 #dxu,dxv sind die Werte der ersten Patielenableitung
                 self.dxu = lambda u,v: rx*(-sin(u))
@@ -48,8 +35,3 @@ class elliptischer_Zylinder(myobject):
                 self.dzvu = lambda u,v: zeros_like(u)
                 self.dzuv = lambda u,v: zeros_like(v)
                 self.dzvv = lambda u,v: zeros_like(v)
-                
-##                 self.duu = lambda t,tckp : splev(t,tckp,2)[0]
-##                 self.duv = lambda t,tckp : zeros_like(duu(t,tckp))
-##                 self.dvu = lambda t,tckp : zeros_like(dvv(t,tckp))
-##                 self.dvv = lambda t,tckp : splev(t,tckp,2)[1]
