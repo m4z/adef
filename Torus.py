ee04d8898f5e0620d9b6cb9da9d94c642bfcec21
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-04-01 01:21:56 +0200

    Torus.py aufgeräumt.

--- Torus.py
+++ Torus.py
@@ -4,17 +4,15 @@ from numpy import sin, cos, pi, mgrid, zeros_like, ones_like
 class Torus(myobject):
 	def __init__(self,rx,ry,rz):
 		myobject.__init__(self)
-                #############################################################################
+                ################################################################
                 #parameter fuer die flaeche
-## 		u, v = mgrid[-pi:mypi:0.01, -pi:mypi:0.01]
-## 		r1, r2 = 1, 0.3
 		r1, r2 = 1, 0.3
 		self.u_f, self.v_f = mgrid[0:2*pi:180j,0:2*pi:180j]
                 #alle funktionen fuer die flaeche
 		self.x = lambda u,v: rx*(r1+r2*cos(u))*sin(v)
 		self.y = lambda u,v: ry*(r1+r2*cos(u))*cos(v)
 		self.z = lambda u,v: rz*r2*sin(u)
-		#############################################################################
+		################################################################
                 #zeros_like = einfach die arraywerte in 0 umschreiben.
                 #ones_like = einfach die arraywerte in 1 umschreiben.
                 #dxu,dxv sind die Werte der ersten Patielenableitung
