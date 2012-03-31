f5e069e52f528bdb9413d27d3a0629a2b931e3bf
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-04-01 01:11:06 +0200

    hyperbolisches_Paraboloid.py aufgeräumt.

--- hyperbolisches_Paraboloid.py
+++ hyperbolisches_Paraboloid.py
@@ -4,16 +4,14 @@ from numpy import sin, cos, pi, mgrid, zeros_like, ones_like
 class hyperbolisches_Paraboloid(myobject):
 	def __init__(self,rx,ry,rz):
 		myobject.__init__(self)
-                #############################################################################
+                ################################################################
                 #parameter fuer die flaeche
-## 		self.u, self.v = mgrid[0:2*pi:180j,0:2*pi:180j]
-## 		self.u, self.v = mgrid[-pi:pi:180j,-pi:pi:180j]
 		self.u_f, self.v_f = mgrid[-1:1:180j,-1:1:180j]
                 #alle funktionen fuer die flaeche
 		self.x = lambda u,v: rx*u
 		self.y = lambda u,v: ry*v
 		self.z = lambda u,v: rz*(u**2-v**2)
-		#############################################################################
+		################################################################
                 #zeros_like = einfach die arraywerte in 0 umschreiben.
                 #ones_like = einfach die arraywerte in 1 umschreiben.
                 #dxu,dxv sind die Werte der ersten Patielenableitung
