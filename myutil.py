3101ddc00a8700e2f35e3363abb755b5e375e182
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-05-11 13:05:01 +0200

    Catch myobj==None.

--- myutil.py
+++ myutil.py
@@ -24,73 +24,79 @@ def ebene(x, y, z, u1, v1, w1, u2, v2, w2, rgb, ii, myname):
     return mlab.mesh(px, py, pz, color=rgb, opacity=0.5, name=myname)
 
 def punkt(myobj, ii):
-    #Alle Vektoren
-    myobj.tangente.mlab_source.set(x=myobj.xnew[ii],
-                                   y=myobj.ynew[ii],
-                                   z=myobj.znew[ii],
-                                   u=myobj.a_n[ii],
-                                   v=myobj.b_n[ii],
-                                   w=myobj.c_n[ii])
-    myobj.ableitung2.mlab_source.set(x=myobj.xnew[ii],
-                                     y=myobj.ynew[ii],
-                                     z=myobj.znew[ii],
-                                     u=myobj.d_n[ii],
-                                     v=myobj.e_n[ii],
-                                     w=myobj.f_n[ii])
-    myobj.hauptnormale.mlab_source.set(x=myobj.xnew[ii],
-                                       y=myobj.ynew[ii],
-                                       z=myobj.znew[ii],
-                                       u=myobj.j_n[ii],
-                                       v=myobj.k_n[ii],
-                                       w=myobj.l_n[ii])
-    myobj.binormale.mlab_source.set(x=myobj.xnew[ii],
-                                    y=myobj.ynew[ii],
-                                    z=myobj.znew[ii],
-                                    u=myobj.g_n[ii],
-                                    v=myobj.h_n[ii],
-                                    w=myobj.i_n[ii])
-    myobj.tangente_fu.mlab_source.set(x=myobj.xnew[ii],
-                                      y=myobj.ynew[ii],
-                                      z=myobj.znew[ii],
-                                      u=myobj.u1_n[ii],
-                                      v=myobj.u2_n[ii],
-                                      w=myobj.u3_n[ii])
-    myobj.tangente_fv.mlab_source.set(x=myobj.xnew[ii],
-                                      y=myobj.ynew[ii],
-                                      z=myobj.znew[ii],
-                                      u=myobj.v1_n[ii],
-                                      v=myobj.v2_n[ii],
-                                      w=myobj.v3_n[ii])
-    myobj.normale_f.mlab_source.set(x=myobj.xnew[ii],
-                                    y=myobj.ynew[ii],
-                                    z=myobj.znew[ii],
-                                    u=myobj.uv1_n[ii],
-                                    v=myobj.uv2_n[ii],
-                                    w=myobj.uv3_n[ii])
-    myobj.normalkruemmung.mlab_source.set(x=myobj.xnew[ii],
+    if myobj is not None:
+        #Alle Vektoren
+        if myobj.tangente is not None:
+            myobj.tangente.mlab_source.set(x=myobj.xnew[ii],
+                                           y=myobj.ynew[ii],
+                                           z=myobj.znew[ii],
+                                           u=myobj.a_n[ii],
+                                           v=myobj.b_n[ii],
+                                           w=myobj.c_n[ii])
+        else:
+            print "Warning: myutil.punkt: myobj.tangente was None."
+        myobj.ableitung2.mlab_source.set(x=myobj.xnew[ii],
+                                         y=myobj.ynew[ii],
+                                         z=myobj.znew[ii],
+                                         u=myobj.d_n[ii],
+                                         v=myobj.e_n[ii],
+                                         w=myobj.f_n[ii])
+        myobj.hauptnormale.mlab_source.set(x=myobj.xnew[ii],
+                                           y=myobj.ynew[ii],
+                                           z=myobj.znew[ii],
+                                           u=myobj.j_n[ii],
+                                           v=myobj.k_n[ii],
+                                           w=myobj.l_n[ii])
+        myobj.binormale.mlab_source.set(x=myobj.xnew[ii],
+                                        y=myobj.ynew[ii],
+                                        z=myobj.znew[ii],
+                                        u=myobj.g_n[ii],
+                                        v=myobj.h_n[ii],
+                                        w=myobj.i_n[ii])
+        myobj.tangente_fu.mlab_source.set(x=myobj.xnew[ii],
                                           y=myobj.ynew[ii],
                                           z=myobj.znew[ii],
-                                          u=myobj.kn_nf[0][ii],
-                                          v=myobj.kn_nf[1][ii],
-                                          w=myobj.kn_nf[2][ii],
-                                          scale_factor=myobj.k_nf[ii])
-    myobj.geodaetischekruemmung.mlab_source.set(x=myobj.xnew[ii],
-                                                y=myobj.ynew[ii],
-                                                z=myobj.znew[ii],
-                                                u=myobj.kg[0][ii],
-                                                v=myobj.kg[1][ii],
-                                                w=myobj.kg[2][ii])
-    #Alle Ebenen
-    #pxn[..., 0] = pxn[:, :, 0] = pxn.T[0].T
-    myobj.normalebene.mlab_source.set(x=myobj.epxn[..., ii],
-                                      y=myobj.epyn[..., ii],
-                                      z=myobj.epzn[..., ii])
-    myobj.schmiegebene.mlab_source.set(x=myobj.epxs[..., ii],
-                                       y=myobj.epys[..., ii],
-                                       z=myobj.epzs[..., ii])
-    myobj.rektifizierendeebene.mlab_source.set(x=myobj.epxr[..., ii],
-                                               y=myobj.epyr[..., ii],
-                                               z=myobj.epzr[..., ii])
-    myobj.tangentialebene_f.mlab_source.set(x=myobj.epxt[..., ii],
-                                            y=myobj.epyt[..., ii],
-                                            z=myobj.epzt[..., ii])
+                                          u=myobj.u1_n[ii],
+                                          v=myobj.u2_n[ii],
+                                          w=myobj.u3_n[ii])
+        myobj.tangente_fv.mlab_source.set(x=myobj.xnew[ii],
+                                          y=myobj.ynew[ii],
+                                          z=myobj.znew[ii],
+                                          u=myobj.v1_n[ii],
+                                          v=myobj.v2_n[ii],
+                                          w=myobj.v3_n[ii])
+        myobj.normale_f.mlab_source.set(x=myobj.xnew[ii],
+                                        y=myobj.ynew[ii],
+                                        z=myobj.znew[ii],
+                                        u=myobj.uv1_n[ii],
+                                        v=myobj.uv2_n[ii],
+                                        w=myobj.uv3_n[ii])
+        myobj.normalkruemmung.mlab_source.set(x=myobj.xnew[ii],
+                                              y=myobj.ynew[ii],
+                                              z=myobj.znew[ii],
+                                              u=myobj.kn_nf[0][ii],
+                                              v=myobj.kn_nf[1][ii],
+                                              w=myobj.kn_nf[2][ii],
+                                              scale_factor=myobj.k_nf[ii])
+        myobj.geodaetischekruemmung.mlab_source.set(x=myobj.xnew[ii],
+                                                    y=myobj.ynew[ii],
+                                                    z=myobj.znew[ii],
+                                                    u=myobj.kg[0][ii],
+                                                    v=myobj.kg[1][ii],
+                                                    w=myobj.kg[2][ii])
+        #Alle Ebenen
+        #pxn[..., 0] = pxn[:, :, 0] = pxn.T[0].T
+        myobj.normalebene.mlab_source.set(x=myobj.epxn[..., ii],
+                                          y=myobj.epyn[..., ii],
+                                          z=myobj.epzn[..., ii])
+        myobj.schmiegebene.mlab_source.set(x=myobj.epxs[..., ii],
+                                           y=myobj.epys[..., ii],
+                                           z=myobj.epzs[..., ii])
+        myobj.rektifizierendeebene.mlab_source.set(x=myobj.epxr[..., ii],
+                                                   y=myobj.epyr[..., ii],
+                                                   z=myobj.epzr[..., ii])
+        myobj.tangentialebene_f.mlab_source.set(x=myobj.epxt[..., ii],
+                                                y=myobj.epyt[..., ii],
+                                                z=myobj.epzt[..., ii])
+    else:
+        print "Warning: myutil.punkt: myobj was None."
