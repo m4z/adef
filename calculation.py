5c29ce6400768a5888e2e0371f767b05fa43d701
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-05-14 00:30:58 +0200

    Comments.

--- calculation.py
+++ calculation.py
@@ -21,15 +21,17 @@ duv = lambda t, tckp : zeros_like(duu(t, tckp))
 dvu = lambda t, tckp : zeros_like(dvv(t, tckp))
 dvv = lambda t, tckp : splev(t, tckp, 2)[1]
 
-#Laenge eines Vektors
+#Laenge eines Vektors (vector length).
 def vektorlaenge(x, y, z):
     laenge = sqrt(x**2 + y**2 + z**2)
     return laenge
 
+# vector direction.
 def vektorausrichtung(x, y, z):
     richtung = x+y+z
     return richtung
 
+# positive vector direction.
 def positivevektorausrichtung(x, y, z):
     matrix1 = abs(vektorausrichtung(x, y, z))/vektorausrichtung(x, y, z)
     x, y, z = array([x, y, z]) * matrix1
@@ -44,15 +46,18 @@ def func(index_t, G, H, I, du, dv, du2, dv2):
                 + (I[0][index_t] + I[1][index_t] + I[2][index_t])
                 * dv2[index_t])
 
+# TODO
 def sf(t, index_t, G, H, I, du, dv, du2, dv2):
     return quad(func, 0, t[index_t], args=(G, H, I, du, dv, du2, dv2))[0]
 
+# TODO
 def bogenlaenge(t, index_t, G, H, I, du, dv, du2, dv2):
     bogenlaenge_werte = zeros_like(t)
     for ii in index_t:
         bogenlaenge_werte[ii] = sf(t, index_t[ii], G, H, I, du, dv, du2, dv2)
     return bogenlaenge_werte
 
+# TODO
 def func2(index_t, G, H, I, du, dv, du2, dv2):
     return sqrt((G[0][index_t] + G[1][index_t] + G[2][index_t])
                 * du2[index_t]
@@ -61,6 +66,7 @@ def func2(index_t, G, H, I, du, dv, du2, dv2):
                 + (I[0][index_t] + I[1][index_t] + I[2][index_t])
                 * dv2[index_t])
 
+# TODO
 def sf2(t, index_t, G, H, I, du, dv, du2, dv2):
     if index_t > 0:
         index_tt = index_t - 1
@@ -71,6 +77,7 @@ def sf2(t, index_t, G, H, I, du, dv, du2, dv2):
                 t[index_t],
                 args=(G, H, I, du, dv, du2, dv2))[0]
 
+# TODO
 def bogenlaenge2(t, index_t, G, H, I, du, dv, du2, dv2):
     bogenlaenge_werte = zeros_like(t)
     for ii in index_t:
@@ -78,19 +85,23 @@ def bogenlaenge2(t, index_t, G, H, I, du, dv, du2, dv2):
     return bogenlaenge_werte
 
 #Normalisierung eines Vektors
+# TODO
 def normalisiere(a, b, c):
     abc = sqrt(a**2 + b**2 + c**2)
     a_n, b_n, c_n = a/abc, b/abc, c/abc
     return a_n, b_n, c_n
 
 #Kreuzprodukt von zwei Vektoren
+# TODO
 def kreuzprodukt(t1, t2, t3, h1, h2, h3):
     return cross(array([t1, t2, t3]).T, array([h1, h2, h3]).T).T
 
+# TODO
 def spatprodukt(v1, v2, v3, v11, v12, v13, v21, v22, v23):
     return kreuzprodukt(v1, v2, v3, v11, v12, v13) * array([v21, v22, v23])
 
 #Eckpunkte einer Ebene
+# TODO
 def eckpunkte_ebene(x, y, z, u1, v1, w1, u2, v2, w2):
     px1, py1, pz1 = [u1+u2+x, x], [v1+v2+y, y], [w1+w2+z, z]
     px2, py2, pz2 = [u1-u2+x, x], [v1-v2+y, y], [w1-w2+z, z]
