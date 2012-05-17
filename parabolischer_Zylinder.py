ae0f24e05f12256156c2b7329a548a237d160d44
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-05-17 16:07:03 +0200

    Comments, imports.

--- parabolischer_Zylinder.py
+++ parabolischer_Zylinder.py
@@ -1,22 +1,23 @@
 from myobject import myobject
-from numpy import sin, cos, pi, mgrid, zeros_like, ones_like
+#from numpy import sin, cos, pi, mgrid, zeros_like, ones_like
+from numpy import mgrid, zeros_like, ones_like
 
 class parabolischer_Zylinder(myobject):
     def __init__(self, rx, ry, rz):
         myobject.__init__(self)
-        ################################################################
-        #parameter fuer die flaeche
+
+        # parameters for the extent (mesh grid)
         self.u_f, self.v_f = mgrid[-1:1:180j, -1:1:180j]
-        #alle funktionen fuer die flaeche
+        # the functions for the extent
         self.x = lambda u, v: rx*u
         self.y = lambda u, v: ry*u**2
         self.z = lambda u, v: rz*v
-        ################################################################
-        #x,y,z sind die Koordinaten im dreidimensionalem euklidischen Raum
-        #ones_like = einfach die arraywerte in 1 umschreiben.
-        #dxu,dxv sind die Werte der ersten Patielenableitung
+
+        # x,y,z sind die Koordinaten im dreidimensionalem euklidischen Raum
+        # ones_like = einfach die arraywerte in 1 umschreiben.
+        # dxu,dxv sind die Werte der ersten Patielenableitung
         self.dxu = lambda u, v: rx*ones_like(u)
-        #zeros_like = einfach die arraywerte in 0 umschreiben.
+        # zeros_like = einfach die arraywerte in 0 umschreiben.
         self.dxv = lambda u, v: zeros_like(v)
         self.dyu = lambda u, v: ry*2*u
         self.dyv = lambda u, v: zeros_like(v)
@@ -26,7 +27,7 @@ class parabolischer_Zylinder(myobject):
         self.dxvu = lambda u, v: zeros_like(u)
         self.dxuv = lambda u, v: zeros_like(v)
         self.dxvv = lambda u, v: zeros_like(v)
-        
+
         self.dyuu = lambda u, v: ry*2
         self.dyvu = lambda u, v: zeros_like(u)
         self.dyuv = lambda u, v: zeros_like(v)
